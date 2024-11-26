22a88fb0f2b296aab48df1309a339ee0dd835df9
ahmadwyne <168010356+ahmadwyne@users.noreply.github.com>
2024-11-26 20:48:34 +0500

    Add files via upload

---
+++ TestCollaborator/2d_Matrix.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Function to print a 2D matrix
+void printMatrix(int matrix[][3], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    // Example 2D matrix with fixed size 3x3
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    // Print the 2D matrix
+    cout << "The matrix is:" << endl;
+    printMatrix(matrix, 3, 3);
+
+    return 0;
+}
